1a75884b2612f1872e6ee21c84856a8477add023
PAPA <sachinxxxsanatani@gmail.com>
2024-08-16 01:10:07 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gunjan890/Spotify_Music",
+    "https://github.com/Kaleiebebewueu/LAMD-KA-CHODA",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
