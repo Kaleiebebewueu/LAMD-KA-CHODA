be91f26172c924092f4f40db55c987aa48b9eae3
PAPA <sachinxxxsanatani@gmail.com>
2024-08-16 08:27:13 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- Spotify_Music/plugins/BAD(PB05)/alive.py
+++ Spotify_Music/plugins/BAD(PB05)/alive.py
@@ -9,22 +9,22 @@ from config import MUSIC_BOT_NAME
 @app.on_message(filters.command(["alive"]))
 async def start(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://telegra.ph/file/662a3c5de42c1cd501607.mp4",
-        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━❄",
+        video=f"https://telegra.ph/file/90a894dff3b1964d0df66.mp4",
+        caption=f"❖ ʜᴇʏ {message.from_user.mention}\n❖ ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n",
         reply_markup=InlineKeyboardMarkup(
             [
                [
             InlineKeyboardButton(
-                text="☆ ɢʀᴏᴜᴘ 💗 ", url=f"https://t.me/ll_BAD_MUNDA_ll"
+                text="ɢʀᴏᴜᴘ", url=f"https://t.me/+Ckzm2ypQyIIzZTll"
             ),
             
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/PBX_PERMOT"
+                text="ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/All_SANATANI_BOT"
             ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+                        "• ᴄʟᴏsᴇ •", callback_data="close"
                     )
                 ],
             ]
